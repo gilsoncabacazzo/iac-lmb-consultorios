f22f865535ea19b397a8fe55c5366a476feb84be
gilsoncabacazzo <gilsoncaballero@gmail.com>
2026-06-26 11:14:43 -0300

    commit inicial

--- index.mjs
+++ index.mjs
@@ -137,12 +137,6 @@ exports.handler = async (event) => {
 const respuesta = (statusCode, body) => {
   return {
     statusCode,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-      "Access-Control-Allow-Methods": "GET,POST,PUT,OPTIONS"
-    },
     body: JSON.stringify(body)
   };
 };
